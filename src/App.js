549a4c63aacac4fdd7f8baf8864945695c9fd6d7
Mamirov1997 <mamirovjavoxir@gmail.com>
2021-10-27 06:04:37 -0700

    'init'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Main from './pages/main';
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 function App() {
   return (
     <Router>
